01e255b28a29287a9ddc6dbc37eda674e4f38165
winty <18814127231@163.com>
2016-08-03 23:45:33 +0800

    singlePic

--- js/singlePic.js
+++ js/singlePic.js
@@ -1,5 +1,16 @@
-function setPreviewPic(fileObj,preview,picWrap,width,height){
+/**
+ * @author winty
+ * @version 1.0.0
+ * @description 纯前端图片预览组件
+ */
+ ;(function(){
+  function setPreviewPic(fileObj,preview,picWrap,maxImgSize,width,height){
+  var pattern=new RegExp('\.(jpg|png|jpeg)+$','i');
+
     if(fileObj.files && fileObj.files[0]){
+      //判断图片格式是否正确
+      if(pattern.test(fileObj.files[0].name)){
+        if(judgeSize(fileObj.files[0].size/1024),maxImgSize){
         //火狐下，直接设置img的样式
         preview.style.display='block';
         preview.style.width=width+'px';
@@ -7,6 +18,14 @@ function setPreviewPic(fileObj,preview,picWrap,width,height){
         
         //火狐7以上版本不能用getAsDataURL()
         preview.src=window.URL.createObjectURL(fileObj.files[0]);
+        }else{
+        alert('你上传的图片太大！');
+        }
+
+      }else{
+        fileObj.files[0]=[];
+        alert('你上传的好像不是图片哦，请检查！');
+      }
     }else{
         //IE下，使用滤镜
         fileObj.select();
@@ -18,14 +37,23 @@ function setPreviewPic(fileObj,preview,picWrap,width,height){
 
      //图片异常的捕捉，防止用户修改非图片的后缀来伪造图片 
         try{
-        	picWrap.style.filter='progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)';
+          picWrap.style.filter='progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)';
             picWrap.filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src=imgSrc;
        }catch(e){
-       	alert('你上传的图片格式有误,请重新选择！');
-       	return false;
+        alert('你上传的图片格式有误,请重新选择！');
+        return false;
        }
        preview.style.display='none';
        document.selection.empty();
     }
     return true;
 }
+   function judgeSize(fileSize,maxSize){
+      if(fileSize>maxSize){
+        return false;
+      }else{
+        return true;
+      }
+   }
+window['setPreviewPic']=setPreviewPic;
+ })()
