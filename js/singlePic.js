220083f828cf1c7a67256b7321ceccdbf884cf18
winty <18814127231@163.com>
2016-08-06 13:36:28 +0800

    finished V_1.1

--- js/singlePic.js
+++ js/singlePic.js
@@ -37,6 +37,10 @@
         this.fileObj.select();
         if(document.selection){
         	var imgSrc=document.selection.createRange().text;
+        	var image=new Image();        
+        	image.src=imgSrc;        
+        	filesize=image.fileSize;  
+        	if(judgeSize(image.fileSize/1024,this.maxImgSize)){
         //IE下必须设置包裹照片的div的大小
         this.picWrap.style.width=width+'px';
         this.picWrap.style.height=height+'px';
@@ -50,7 +54,10 @@
         }
         this.preview.style.display='none';
         document.selection.empty();
+    }else{
+    	alert('你上传的图片太大！');
     }
+}
 }
 return true;
 }
@@ -66,7 +73,7 @@ SetPreviewPic.prototype.uploadPics=function(width,height,maxPics,maxSize){
               	html='';
               }
               if(this.pattern.test(fileObj.files[0].name)){
-              	if(judgeSize(fileObj.files[0].size/1024),this.maxImgSize){
+              	if(judgeSize(fileObj.files[0].size/1024,this.maxImgSize)){
               		html='<img class="newLoad" style="margin:5px;width:'+width+'px;height:'+height+'px;" src='+window.URL.createObjectURL(this.fileObj.files[0])+' />'+html;
               		this.picWrap.innerHTML=html;
               	}else{
@@ -81,11 +88,16 @@ SetPreviewPic.prototype.uploadPics=function(width,height,maxPics,maxSize){
       }else{
         //IE下
         var nums=this.picWrap.childNodes.length;
-    
+
         if(nums<maxPics+2){
-        this.fileObj.select();
-        if(document.selection){
-        	var imgSrc=document.selection.createRange().text;
+        	this.fileObj.select();
+        	if(document.selection){
+        		var imgSrc=document.selection.createRange().text;
+
+        		var image=new Image();        
+        		image.src=imgSrc;        
+        		filesize=image.fileSize;  
+        		if(judgeSize(image.fileSize/1024,this.maxImgSize)){
 
         //IE下必须设置div的大小
         var ele=document.createElement('div');
@@ -101,14 +113,18 @@ SetPreviewPic.prototype.uploadPics=function(width,height,maxPics,maxSize){
         }
         this.preview.style.display='none';
         document.selection.empty();
-        }
     }else{
-    	alert('最多可上传'+maxPics+'张图片！');
+    	alert('你上传的图片太大！');
     }
+}
+}else{
+	alert('最多可上传'+maxPics+'张图片！');
+}
 }
 }
 return true;
 }
+
 function judgeSize(fileSize,maxSize){
 	if(fileSize>maxSize){
 		return false;
